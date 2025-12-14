dba1d29e9f760e2f2fd3a3835a2eec7193d24b9a
dm1trr1y <dimabobro7@gmail.com>
2025-12-15 00:45:55 +0100

    add preloader markup, styles, js. to be fixed

---
+++ src/js/preloader.js
@@ -0,0 +1,10 @@
+// window.addEventListener('load', function () {
+//   var loader = document.querySelector('.js-preloader');
+//   if (loader) {
+//     loader.style.transition = 'opacity 1s ease-in-out';
+//     loader.style.opacity = '0';
+//     this.setTimeout(function () {
+//       loader.style.display = 'none';
+//     }, 6000);
+//   }
+// });
