556e7189fbf8b65d0d35e295c91aa8c4e165734e
dm1trr1y <dimabobro7@gmail.com>
2025-12-20 19:10:23 +0100

    hotfix 1

--- src/js/stories.js
+++ src/js/stories.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const raty = document.querySelectorAll('.star-rating').forEach(box => {
       const ratyBox = new Raty(box, {
         readOnly: true,
-        path: '/img',
+        path: '/src/img',
         starHalf: 'star-half.png',
         starOn: 'star-filled.png',
         starOff: 'star-outline.png',
